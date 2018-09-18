ed9e007bb717cd04176ec3589938a3c9f7f08026
Sean <sgroff04@gmail.com>
2018-09-18 00:09:40 -0500

    init commit

---
+++ src/components/HistoryTableDK.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-table
+      :items="items"
+      striped
+      hover/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HistoryTableDK',
+  props: {
+    items: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar />
+    <b-jumbotron
+      header="Bootstrap Vue"
+      lead="Bootstrap 4 Components for Vue.js 2" >
+      <p>For more information visit website</p>
+      <b-btn-group size="lg">
+        <b-btn variant="primary">Entries {{ entries }}</b-btn>
+        <b-btn variant="info">Fees Paid ${{ feesPaid | currency }}</b-btn>
+        <b-btn variant="success">Profit ${{ profit | currency }}</b-btn>
+        <b-btn variant="success">ROI {{ profit | currency }}%</b-btn>
+      </b-btn-group>
+    </b-jumbotron>
+    <history-table-d-k :items="data" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+const data = [
+  { id: 1, contestName: 'NFL Pylon 9000', won: 0, entryFee: 1.0 },
+  { id: 2, contestName: 'NFL Spike 2k', won: 1.8, entryFee: 1.0 },
+  { id: 3, contestName: 'NFL Huddle', won: 5.0, entryFee: 1.0 },
+  { id: 4, contestName: 'NFL Smash', won: 2.6, entryFee: 2.0 },
+  { id: 5, contestName: 'NFL Spike', won: 0, entryFee: 2.0 },
+  { id: 6, contestName: 'NFL Gauntlet', won: 0, entryFee: 1.0 },
+  { id: 7, contestName: 'NFL Oklahoma Drill', won: 1.8, entryFee: 1.0 },
+  { id: 8, contestName: 'NFL H2H', won: 0, entryFee: 1.0 },
+  { id: 9, contestName: 'NFL 50/50', won: 0, entryFee: 1.0 },
+]
+import NavBar from '@/components/BaseNavBar.vue'
+import HistoryTableDK from '@/components/HistoryTableDK.vue'
 
 export default {
-  name: "home",
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-};
+    NavBar,
+    HistoryTableDK,
+  },
+  filters: {
+    currency(value) {
+      if (!value || value === 0) return
+
+      return value.toFixed(2)
+    },
+  },
+  data() {
+    return {
+      data,
+    }
+  },
+  computed: {
+    entries() {
+      return data.length
+    },
+    feesPaid() {
+      return data.reduce((accum, item) => (accum += item.entryFee), 0)
+    },
+    profit() {
+      return data.reduce((accum, item) => (accum += item.won), 0) - this.feesPaid
+    },
+    roi() {
+      return (this.profit / this.feesPaid) * 100
+    },
+  },
+}
 </script>
